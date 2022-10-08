7ab708d2918b8b7f98ffb4baac511b434fb72e7b
Дмитрий Горбунов <bous07@mail.ru>
2022-10-08 20:34:08 +0300

    creating the Data Storage class

---
+++ MobileBank/Classes/DataStorage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileBank.Classes
+{
+    class DataStorage
+    {
+        public static int attempts;
+        public static string bankCard;
+        public static string idClient;
+        public static string phoneNumber;
+        public static string cardNumber;
+        public static string cardCW;
+    }
+}
