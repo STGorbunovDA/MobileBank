69935248b2d6cf8d2a0543dc71e5eb92622f0fa3
Дмитрий Горбунов <bous07@mail.ru>
2022-10-11 17:39:31 +0300

    Shape design

--- MobileBank/Forms/Form1.Designer.cs
+++ MobileBank/Forms/Form1.Designer.cs
@@ -28,20 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_closeLoginForm = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lbL_enterNumberPhone = new System.Windows.Forms.Label();
+            this.lbL_enterPassword = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btn_closeLoginForm
+            // 
+            this.btn_closeLoginForm.BackColor = System.Drawing.Color.GreenYellow;
+            this.btn_closeLoginForm.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_closeLoginForm.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_closeLoginForm.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_closeLoginForm.Location = new System.Drawing.Point(368, 13);
+            this.btn_closeLoginForm.Name = "btn_closeLoginForm";
+            this.btn_closeLoginForm.Size = new System.Drawing.Size(24, 23);
+            this.btn_closeLoginForm.TabIndex = 0;
+            this.btn_closeLoginForm.Text = "X";
+            this.btn_closeLoginForm.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(178, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Вход";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(12, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите номер телефона";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(12, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Введите пароль";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(380, 34);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 262);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(380, 34);
+            this.textBox2.TabIndex = 5;
+            // 
+            // lbL_enterNumberPhone
+            // 
+            this.lbL_enterNumberPhone.AutoSize = true;
+            this.lbL_enterNumberPhone.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbL_enterNumberPhone.ForeColor = System.Drawing.Color.GreenYellow;
+            this.lbL_enterNumberPhone.Location = new System.Drawing.Point(12, 118);
+            this.lbL_enterNumberPhone.Name = "lbL_enterNumberPhone";
+            this.lbL_enterNumberPhone.Size = new System.Drawing.Size(387, 16);
+            this.lbL_enterNumberPhone.TabIndex = 6;
+            this.lbL_enterNumberPhone.Text = "................................................................................." +
+    "..............";
+            // 
+            // lbL_enterPassword
+            // 
+            this.lbL_enterPassword.AutoSize = true;
+            this.lbL_enterPassword.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbL_enterPassword.ForeColor = System.Drawing.Color.GreenYellow;
+            this.lbL_enterPassword.Location = new System.Drawing.Point(12, 203);
+            this.lbL_enterPassword.Name = "lbL_enterPassword";
+            this.lbL_enterPassword.Size = new System.Drawing.Size(387, 16);
+            this.lbL_enterPassword.TabIndex = 7;
+            this.lbL_enterPassword.Text = "................................................................................." +
+    "..............";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(443, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(404, 381);
+            this.Controls.Add(this.lbL_enterPassword);
+            this.Controls.Add(this.lbL_enterNumberPhone);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_closeLoginForm);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_closeLoginForm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lbL_enterNumberPhone;
+        private System.Windows.Forms.Label lbL_enterPassword;
     }
 }
 
