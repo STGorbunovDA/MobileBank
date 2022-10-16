1545c2e1e24223c88cc7848de58015e45df661e0
Дмитрий Горбунов <bous07@mail.ru>
2022-10-16 22:41:33 +0800

    Saving the code

--- MobileBank/Forms/MainForm.Designer.cs
+++ MobileBank/Forms/MainForm.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_closeLoginForm = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.btn_closeLoginForm);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 46);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_closeLoginForm
+            // 
+            this.btn_closeLoginForm.BackColor = System.Drawing.Color.GreenYellow;
+            this.btn_closeLoginForm.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_closeLoginForm.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_closeLoginForm.ForeColor = System.Drawing.Color.Gray;
+            this.btn_closeLoginForm.Location = new System.Drawing.Point(764, 12);
+            this.btn_closeLoginForm.Name = "btn_closeLoginForm";
+            this.btn_closeLoginForm.Size = new System.Drawing.Size(24, 23);
+            this.btn_closeLoginForm.TabIndex = 1;
+            this.btn_closeLoginForm.Text = "X";
+            this.btn_closeLoginForm.UseVisualStyleBackColor = false;
+            this.btn_closeLoginForm.Click += new System.EventHandler(this.Btn_closeLoginForm_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseUp);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_closeLoginForm;
     }
 }
--- MobileBank/Forms/MainForm.cs
+++ MobileBank/Forms/MainForm.cs
@@ -12,9 +12,39 @@ namespace MobileBank.Forms
 {
     public partial class MainForm : Form
     {
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            dragCursorPoint = Cursor.Position;
+            dragFormPoint = this.Location;
+        }
+
+        void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point dif = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(dif));
+            }
+        }
+
+        void MainForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+        void Btn_closeLoginForm_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
         }
     }
 }
