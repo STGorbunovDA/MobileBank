f6a52728710daa0dd2d78e59def6436ad68d0820
Дмитрий Горбунов <bous07@mail.ru>
2022-10-24 03:34:04 +0800

    save code

--- MobileBank/Forms/MobilePhoneTransferForm.cs
+++ MobileBank/Forms/MobilePhoneTransferForm.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MobileBank.Classes;
+using System;
 using System.Drawing;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace MobileBank.Forms
@@ -9,10 +11,14 @@ namespace MobileBank.Forms
         private bool dragging = false;
         private Point dragCursorPoint;
         private Point dragFormPoint;
+        Random rand = new Random();
 
         public MobilePhoneTransferForm()
         {
             InitializeComponent();
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
             StartPosition = FormStartPosition.CenterScreen;
         }
 
@@ -40,6 +46,284 @@ namespace MobileBank.Forms
         void MobilePhoneTransferForm_MouseUp(object sender, MouseEventArgs e)
         {
             dragging = false;
+        }
+
+        void MobilePhoneTransferForm_Load(object sender, EventArgs e)
+        {
+            txB_card_numberUser.Text = "";
+            int tmp = 0;
+            int tmp1 = 4;
+            for (int m = 0; m < 4; m++)
+            {
+                for (int n = tmp; n < tmp1; n++)
+                {
+                    txB_card_numberUser.Text += DataStorage.cardNumberUser[n].ToString();
+                }
+                txB_card_numberUser.Text += " ";
+                tmp += 4;
+                tmp1 += 4;
+            }
+            txB_transferMobilePhone.Text = DataStorage.phoneNumber;
+            lbl_currency.Text = DataStorage.currency;
+            txB_card_numberUser.Text = txB_card_numberUser.Text.Trim();
+            txB_cardDate.Text = DataStorage.cardDate;
+            txB_cardCvv.Text = "";
+
+            SettingMethod.Loading_serviceTypeMobile_cmd(cmb_serviceTypeMobileOperator);
+
+            txB_transferMobilePhone.Select();
+        }
+
+        void TxB_sumTransferPhone_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0' && e.KeyChar <= '9') || e.KeyChar == (char)Keys.Back)
+            {
+
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TxB_sumTransferPhone_TextChanged_1(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(txB_sum.Text))
+            {
+                if (lbl_currency.Text == "RUB")
+                {
+                    if (Convert.ToInt32(txB_sum.Text) > 49)
+                    {
+                        lbL_commissionTransferPhone.Text = (Convert.ToDouble(txB_sum.Text) / 100 * DataStorage.commissionPhoneTransfer).ToString();
+                        lbL_paymentTransferPhone.Text = (Convert.ToDouble(lbL_commissionTransferPhone.Text) + Convert.ToDouble(txB_sum.Text)).ToString();
+                    }
+                    else 
+                    {
+                        lbL_commissionTransferPhone.Text = "";
+                        lbL_paymentTransferPhone.Text = "";
+                    }
+                }
+                else if (lbl_currency.Text == "USD")
+                {
+                    if (Convert.ToDouble(txB_sum.Text) > 0.99)
+                    {
+                        var reg2 = new Regex(",");
+                        double dolar = Convert.ToDouble(reg2.Replace(DataStorage.dolar.ToString(), "."));
+                        lbL_commissionTransferPhone.Text = (((Convert.ToDouble(txB_sum.Text) * dolar) / 100 * DataStorage.commissionPhoneTransfer) / dolar).ToString();
+                        lbL_paymentTransferPhone.Text = (Convert.ToDouble(lbL_commissionTransferPhone.Text) + Convert.ToDouble(txB_sum.Text)).ToString();
+                    }
+                    else
+                    {
+                        lbL_commissionTransferPhone.Text = "";
+                        lbL_paymentTransferPhone.Text = "";
+                    }
+                }
+                else if (lbl_currency.Text == "USD")
+                {
+                    if (Convert.ToDouble(txB_sum.Text) > 0.99)
+                    {
+                        var reg2 = new Regex(",");
+                        double euro = Convert.ToDouble(reg2.Replace(DataStorage.euro.ToString(), "."));
+                        lbL_commissionTransferPhone.Text = (((Convert.ToDouble(txB_sum.Text) * euro) / 100 * DataStorage.commissionPhoneTransfer) / euro).ToString();
+                        lbL_paymentTransferPhone.Text = (Convert.ToDouble(lbL_commissionTransferPhone.Text) + Convert.ToDouble(txB_sum.Text)).ToString();
+                    }
+                    else
+                    {
+                        lbL_commissionTransferPhone.Text = "";
+                        lbL_paymentTransferPhone.Text = "";
+                    }
+                }
+            } 
+            else
+            {
+                lbL_commissionTransferPhone.Text = "";
+                lbL_paymentTransferPhone.Text = "";
+            }
+        }
+
+        void Btn_TransferPhone_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MessageBoxButtons btn = MessageBoxButtons.OK;
+                MessageBoxIcon ico = MessageBoxIcon.Information;
+
+                string caption = "Отмена. Невозможно осуществить перевод средств";
+                if (String.IsNullOrEmpty(txB_sum.Text))
+                {
+                    if (txB_cardCvv.Text != "")
+                    {
+                        if (!Regex.IsMatch(txB_cardDate.Text, @"^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])"))
+                        {
+                            MessageBox.Show("Введите корректно месяц и дату[mm/yy]", caption, btn, ico);
+                            txB_cardDate.Select();
+                            return;
+                        }
+                        if (DataStorage.cardCVV != txB_cardCvv.Text)
+                        {
+                            MessageBox.Show("Вы ввели не корректный CVV-код", caption, btn, ico);
+                            if (DataStorage.attemptsCVV < 3)
+                                DataStorage.attemptsCVV++;
+                            else
+                            {
+                                MessageBox.Show("Вы ввели неверный CVV-код более трёх раз", caption, btn, ico);
+                                this.Close();
+                            }
+                            txB_cardCvv.Select();
+                            return;
+                        }
+                        if (DataStorage.cardDate != txB_cardDate.Text)
+                        {
+                            MessageBox.Show("Вы ввели не корректный дату Вашей карты", caption, btn, ico);
+                            txB_cardCvv.Select();
+                            return;
+                        }
+                        var reg = new Regex(" ");
+                        var cardNumberUser = reg.Replace(txB_card_numberUser.Text, "");
+
+                        if (DataStorage.cardNumberUser != cardNumberUser)
+                        {
+                            MessageBox.Show("Вы ввели не корректный номер Вашей карты", caption, btn, ico);
+                            txB_cardCvv.Select();
+                            return;
+                        }
+                        if (DataStorage.currency == "RUB")
+                        {
+                            if (Convert.ToDouble(txB_sum.Text) < 49.00)
+                            {
+                                MessageBox.Show("Сумма перевода должна быть не меньше 50 RUB", caption, btn, ico);
+                                txB_sum.Select();
+                                return;
+                            }
+                        }
+                        if (DataStorage.currency == "USD")
+                        {
+                            if (Convert.ToDouble(txB_sum.Text) < 1.00)
+                            {
+                                MessageBox.Show("Сумма перевода должна быть не меньше 1 USD", caption, btn, ico);
+                                txB_sum.Select();
+                                return;
+                            }
+                        }
+                        if (DataStorage.currency == "EUR")
+                        {
+                            if (Convert.ToDouble(txB_sum.Text) < 1.00)
+                            {
+                                MessageBox.Show("Сумма перевода должна быть не меньше 1 EUR", caption, btn, ico);
+                                txB_sum.Select();
+                                return;
+                            }
+                        }
+                        if (Convert.ToDouble(DataStorage.balanceCard) < Convert.ToDouble(lbL_paymentTransferPhone.Text))
+                        {
+                            MessageBox.Show($"Недостаточно денег на Вашем счёте: \"{DataStorage.balanceCard}\"", caption, btn, ico);
+                            txB_sum.Select();
+                            return;
+                        }
+
+                        if (cmb_serviceTypeMobileOperator.Text == "МТС")
+                        {
+                            var phoneOperatorCheack = txB_transferMobilePhone.Text.Substring(3, 2);
+                            if (phoneOperatorCheack != "10" && phoneOperatorCheack != "11" && phoneOperatorCheack != "12" && phoneOperatorCheack != "13"
+                                && phoneOperatorCheack != "14" && phoneOperatorCheack != "15" && phoneOperatorCheack != "16" && phoneOperatorCheack != "17"
+                                && phoneOperatorCheack != "18" && phoneOperatorCheack != "19" && phoneOperatorCheack != "81" && phoneOperatorCheack != "82"
+                                && phoneOperatorCheack != "83" && phoneOperatorCheack != "84" && phoneOperatorCheack != "85" && phoneOperatorCheack != "86"
+                                && phoneOperatorCheack != "87" && phoneOperatorCheack != "88" && phoneOperatorCheack != "89")
+                            {
+                                MessageBox.Show($"Введите корректный номер телефона оператора МТС", caption, btn, ico);
+                                txB_transferMobilePhone.Select();
+                                return;
+                            }
+                        }
+                        if (cmb_serviceTypeMobileOperator.Text == "Мегафон")
+                        {
+                            var phoneOperatorCheack = txB_transferMobilePhone.Text.Substring(3, 2);
+                            if (phoneOperatorCheack != "24" && phoneOperatorCheack != "29" && phoneOperatorCheack != "34" && phoneOperatorCheack != "28"
+                                && phoneOperatorCheack != "29" && phoneOperatorCheack != "38" && phoneOperatorCheack != "27" && phoneOperatorCheack != "29"
+                                && phoneOperatorCheack != "38" && phoneOperatorCheack != "21" && phoneOperatorCheack != "31" && phoneOperatorCheack != "23"
+                                && phoneOperatorCheack != "83" && phoneOperatorCheack != "84" && phoneOperatorCheack != "85" && phoneOperatorCheack != "86"
+                                && phoneOperatorCheack != "33" && phoneOperatorCheack != "25" && phoneOperatorCheack != "26" && phoneOperatorCheack != "36" 
+                                && phoneOperatorCheack != "22" && phoneOperatorCheack != "32" && phoneOperatorCheack != "20" && phoneOperatorCheack != "30")
+                            {
+                                MessageBox.Show($"Введите корректный номер телефона оператора Мегафон", caption, btn, ico);
+                                txB_transferMobilePhone.Select();
+                                return;
+                            }
+                        }
+                        if (cmb_serviceTypeMobileOperator.Text == "Билайн")
+                        {
+                            var phoneOperatorCheack = txB_transferMobilePhone.Text.Substring(3, 2);
+                            if (phoneOperatorCheack != "03" && phoneOperatorCheack != "05" && phoneOperatorCheack != "06" && phoneOperatorCheack != "09"
+                                && phoneOperatorCheack != "60" && phoneOperatorCheack != "61" && phoneOperatorCheack != "62" && phoneOperatorCheack != "63"
+                                && phoneOperatorCheack != "64" && phoneOperatorCheack != "65" && phoneOperatorCheack != "67" && phoneOperatorCheack != "76"
+                                && phoneOperatorCheack != "01" && phoneOperatorCheack != "02" && phoneOperatorCheack != "08" && phoneOperatorCheack != "66"
+                                && phoneOperatorCheack != "68")
+                            {
+                                MessageBox.Show($"Введите корректный номер телефона оператора Мегафон", caption, btn, ico);
+                                txB_transferMobilePhone.Select();
+                                return;
+                            }
+                        }
+                        if (cmb_serviceTypeMobileOperator.Text == "Теле2")
+                        {
+                            var phoneOperatorCheack = txB_transferMobilePhone.Text.Substring(3, 2);
+                            if (phoneOperatorCheack != "01" && phoneOperatorCheack != "02" && phoneOperatorCheack != "04" && phoneOperatorCheack != "05"
+                                && phoneOperatorCheack != "08" && phoneOperatorCheack != "50" && phoneOperatorCheack != "51" && phoneOperatorCheack != "52"
+                                && phoneOperatorCheack != "53" && phoneOperatorCheack != "58" && phoneOperatorCheack != "11" && phoneOperatorCheack != "99"
+                                && phoneOperatorCheack != "96" && phoneOperatorCheack != "93" && phoneOperatorCheack != "00" && phoneOperatorCheack != "92"
+                                && phoneOperatorCheack != "95")
+                            {
+                                MessageBox.Show($"Введите корректный номер телефона оператора Мегафон", caption, btn, ico);
+                                txB_transferMobilePhone.Select();
+                                return;
+                            }
+                        }
+
+
+                        var reg2 = new Regex(",");
+                        double dolar = Convert.ToDouble(reg2.Replace(DataStorage.dolar.ToString(), "."));
+                        double euro = Convert.ToDouble(reg2.Replace(DataStorage.euro.ToString(), "."));
+
+                        var reg3 = new Regex(" ");
+                        var card_numberUser = reg3.Replace(txB_card_numberUser.Text, "");
+
+                        string sum = lbL_paymentTransferPhone.Text;
+                        var cardCurrencyUser = DataStorage.currency;
+
+                        Validations validations = new Validations();
+                        validations.ShowDialog();
+
+                        if (DataStorage.attemptsPin < 3)
+                        {
+                            if (InternetСheck.CheackSkyNET())
+                            {
+
+                                if(lbl_currency.Text == "RUB")
+                                {
+
+                                }
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        txB_cardCvv.Select();
+                        MessageBox.Show("CVV-код не введён", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else 
+                {
+                    MessageBox.Show("Сумма не должна быть пустой", caption, btn, ico);
+                    txB_sum.Select();
+                    return;
+                }
+               
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка пополнения мобильного (Btn_TransferPhone_Click)", "Ошибка метода", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
